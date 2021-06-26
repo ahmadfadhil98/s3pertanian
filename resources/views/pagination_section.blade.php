a5b1d890b727a19128fe8557f61bab22a567a6e9
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-06-26 11:29:01 +0700

    praja_uh

--- resources/views/pagination_section.blade.php
+++ resources/views/pagination_section.blade.php
@@ -1,79 +1,72 @@
 <div>
     @if ($paginator->hasPages())
-    <div class="px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-        <div class="flex-1 flex justify-between sm:hidden">
-          <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 hover:bg-gray-50">
-            Previous
-          </a>
-          <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 hover:bg-gray-50">
-            Next
-          </a>
-        </div>
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+    <div class="py-4 flex items-center border-t-2 border-gray-200">
+        <div class="mt-1 hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
-            <p class="text-sm text-gray-700">
-              Showing
-              <span class="font-medium">1</span>
-              to
-              <span class="font-medium">10</span>
-              of
-              <span class="font-medium">97</span>
-              results
+            <p class="text-sm text-gray-600">
+              Ditemukan
+              <span class="font-medium font-semibold text-green-500">1</span>
+              hasil
             </p>
           </div>
           <div>
-            <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+            <nav class="relative z-0 inline-flex rounded-xl" aria-label="Pagination">
                 <span>
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
-                        <a class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                            <span class="sr-only">Previous</span>
-                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                              <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        <a class="mx-2 relative inline-flex items-center px-4 py-1 text-gray-400 hover:text-gray-600 bg-white hover:bg-gray-200 leading-5 rounded-xl focus:outline-nonee">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
                             </svg>
                           </a>
                     @else
-                        {{-- <button wire:click="previousPage" wire:loading.attr="disabled" rel="prev" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {!! __('pagination.previous') !!}
-                        </button> --}}
-                        <button wire:click="previousPage" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            <span class="sr-only">Previous</span>
-                            <!-- Heroicon name: solid/chevron-left -->
-                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                              <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        <button wire:click="previousPage" class="mx-2 relative inline-flex items-center px-4 py-1 text-gray-400 hover:text-gray-600 bg-white hover:bg-gray-200 leading-5 rounded-xl focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
                             </svg>
                           </button>
                     @endif
                 </span>
-                @foreach ($elements as $element)
+                <ul>
+                    @foreach ($elements as $element)
                     <div class="flex">
                         @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                        <li class="mx-2 w-10 px-2 py-1 text-center rounded border shadow bg-blue-500 text-white cursor-pointer" wire:click="gotoPage({{$page}})">{{$page}}</li>
-                        @else
-                        <li class="mx-2 w-10 px-2 py-1 text-center rounded border shadow bg-white cursor-pointer" wire:click="gotoPage({{$page}})">{{$page}}</li>
-                        @endif
-                        @endforeach
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    {{-- {{$url}}    --}}
+                                    <li class="w-10 mx-1 px-2 py-1 text-center text-sm rounded-xl bg-yellow-300 text-gray-600 cursor-pointer" wire:click="gotoPage({{$page}})">{{$page}}</li>
+                                @else
+                                    <li class="w-10 mx-1 px-2 py-1 text-center text-sm text-gray-400 hover:text-gray-600 rounded-xl bg-white hover:bg-gray-200 cursor-pointer" wire:click="gotoPage({{$page}})">{{$page}}</li>
+                                @endif
+                            @endforeach
                         @endif
                     </div>
                 @endforeach
+                </ul>
+
 
                 <span>
                     {{-- Next Page Link --}}
                     @if ($paginator->hasMorePages())
-                        <button wire:click="nextPage" wire:loading.attr="disabled" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {!! __('pagination.next') !!}
+                        <button wire:click="nextPage" wire:loading.attr="disabled" rel="next" class="mx-2 relative inline-flex items-center px-4 py-1 text-gray-400 hover:text-gray-600 bg-white hover:bg-gray-200 leading-5 rounded-xl focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+                            </svg>
                         </button>
                     @else
-                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
-                            {!! __('pagination.next') !!}
+                        <span class="mx-2 relative inline-flex items-center px-4 py-1 text-gray-400 hover:text-gray-600 bg-white hover:bg-gray-200 leading-5 rounded-xl focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+                            </svg>
                         </span>
                     @endif
                 </span>
             </nav>
           </div>
         </div>
-      </div>
+    </div>
+    <div class="text-sm text-gray-300 text-center">
+        Sistem Informasi Universitas Andalas
+    </div>
     @endif
 </div>
