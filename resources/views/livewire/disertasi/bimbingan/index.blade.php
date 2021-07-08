a8821945d2e6f5daf4884d564f9c3f9ae1e391ae
prajarisnaldy <prajarisnaldy06@gmail.com>
2021-07-08 19:30:13 +0700

    Praja_njbdhwajhvavdjhawvdajvdgawdvwh

--- resources/views/livewire/disertasi/bimbingan/index.blade.php
+++ resources/views/livewire/disertasi/bimbingan/index.blade.php
@@ -17,15 +17,17 @@
                 </div>
             </div>
 
-            <div class="transform hover:scale-95 duration-300 bg-gray-50 w-full md:w-1/3 flex text-gray-400 pl-5 rounded-xl shadow-inner">
-                @include('search')
+            <div class="flex mt-6">
+                <div class="transform hover:scale-95 duration-300 bg-gray-50 w-full md:w-1/3 flex text-gray-400 pl-5 rounded-xl shadow-inner">
+                    @include('search')
+                </div>
             </div>
 
                 <table class="table-fixed w-full mt-6">
                     <thead>
                         <tr>
-                            <th class="bg-yellow-300 text-base font-bold py-3 text-gray-600 rounded-tl-xl rounded-bl-xl w-20">No</th>
-                            <th class="bg-yellow-300 py-3 text-gray-600 w-20">
+                            <th style="background-color: #057954;" class="font-normal text-base py-2.5 text-white rounded-tl-xl rounded-bl-xl w-20">No</th>
+                            <th style="background-color: #057954;" class="font-normal text-base py-2.5 text-white w-28">
                                 <div class="flex">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
@@ -34,8 +36,8 @@
                                 </div>
                             </th>
 
-                            <th class="bg-yellow-300 py-3 text-gray-600 w-45">
-                                <div class="flex ml-12">
+                            <th style="background-color: #057954;" class="font-normal text-base py-2.5 text-white w-52">
+                                <div class="flex">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                       </svg>
@@ -43,7 +45,7 @@
                                 </div>
                             </th>
 
-                            <th class="bg-yellow-300 py-3 text-gray-600 w-96">
+                            <th style="background-color: #057954;" class="font-normal text-base py-2.5 text-white w-auto">
                                 <div class="flex">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -52,15 +54,15 @@
                                 </div>
                             </th>
 
-                            <th class="bg-yellow-300 py-3 text-gray-600 rounded-tr-xl rounded-br-xl w-40">
-                                <div class="flex ml-10">
+                            <th style="background-color: #057954;" class="font-normal text-base py-2.5 text-white rounded-tr-xl rounded-br-xl w-36">
+                                <div class="flex ml-6">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                       </svg>
                                     <div class="ml-3">Status</div>
                                 </div>
                             </th>
-                            <th class="w-60"></th>
+                            <th class="w-48"></th>
                         </tr>
                     </thead>
 
@@ -69,13 +71,22 @@
                             <tr>
                                 <td class="text-center text-base text-gray-600">{{ $students->firstitem() + $key }}.</td>
                                 <td class="text-left text-base text-gray-600">{{ $stu_nim[$dis_stu[$student->disertasi_id]] }}</td>
-                                <td class="text-left text-base text-gray-600 font-bold px-12">{{ $stu_name[$dis_stu[$student->disertasi_id]] }}</td>
-                                <td class="text-left text-base text-gray-600 font-bold py-2">{{ $disertasi[$student->disertasi_id] }}</td>
-                                <td class="text-left text-base text-gray-600 font-bold px-11">{{ $statuses[$student->approve] }}</td>
+                                <td class="text-left text-base text-gray-600">{{ $stu_name[$dis_stu[$student->disertasi_id]] }}</td>
+                                <td class="text-left text-base text-gray-600 py-2">{{ $disertasi[$student->disertasi_id] }}</td>
+                                <td class="pl-6">
+                                    <span class="flex text-sm text-{{ $colors[$student->approve]}} py-3">
+                                        @php
+                                            echo $icons [$student->approve];
+                                        @endphp
+                                        <button class="pl-2 text-gray-600">
+                                            {{ $statuses [$student->approve] }}
+                                        </button>
+                                    </span>
+                                </td>
                                     @if($student->approve==1)
                                 <td>
                                         <div class="flex">
-                                            <button wire:click="agree({{ $student->id }})" class="flex rounded-xl text-sm font-bold bg-green-500 hover:bg-green-700 text-white py-3 px-5 focus:outline-none shadow-md mr-2.5">
+                                            <button style="background-color: #057954;" wire:click="agree({{ $student->id }})" class="transform hover:scale-95 duration-300 flex rounded-xl text-sm text-white py-3 px-5 focus:outline-none shadow-md mr-2.5">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                   </svg>
@@ -84,7 +95,7 @@
                                                 </div>
                                             </button>
 
-                                            <button wire:click="reject({{ $student->id }})" class="flex rounded-xl text-sm font-bold bg-red-500 hover:bg-red-700 text-white py-3 px-5 focus:outline-none shadow-md">
+                                            <button style="background-color: #E42025;" wire:click="reject({{ $student->id }})" class="transform hover:scale-95 duration-300 flex rounded-xl text-sm text-white py-3 px-5 focus:outline-none shadow-md">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                   </svg>
@@ -97,7 +108,7 @@
                                     </td>
                                     @else
                                     <td class="pl-16">
-                                        <button onclick="location.href=' {{ route( 'ddisertasi',[$student->disertasi_id]) }} '" class="flex rounded-xl text-sm font-bold bg-yellow-300 hover:bg-yellow-400 text-gray-600 py-3 px-5 focus:outline-none shadow-md">
+                                        <button style="background-color: #078CAA;" onclick="location.href=' {{ route( 'ddisertasi',[$student->disertasi_id]) }} '" class="transform hover:scale-95 duration-300 flex rounded-xl text-sm text-white py-3 px-5 focus:outline-none shadow-md">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                               </svg>
