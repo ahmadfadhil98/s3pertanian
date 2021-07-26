49b2dd94135bf9de1413ee6c9223f2dd8cc2e7b9
ahmadfadhil98 <1611522009@student.unand.ac.id>
2021-07-26 20:23:19 +0700

    upload_fileinfileAdil

--- resources/views/livewire/file/form.blade.php
+++ resources/views/livewire/file/form.blade.php
@@ -28,7 +28,7 @@
                         @error('name') <h1 class="text-red-500">{{$message}}</h1>@enderror
                     </div>
 
-                    <div>
+                    {{-- <div>
                         <div class="flex text-sm text-gray-600 font-bold mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -37,7 +37,31 @@
                         </div>
                         <input type="file" wire:model="file" name="file" class="bg-white w-full py-2.5 px-4 text-sm text-gray-400 rounded-xl focus:outline-none shadow-md">
                         @error('file') <h1 class="text-red-500">{{$message}}</h1>@enderror
+                    </div> --}}
+
+                    <div>
+                        <div class="flex text-sm text-gray-600 font-bold mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                            </svg>
+                            <div class="ml-3">File</div>
+                        </div>
+                        <div class="relative h-40 rounded-xl bg-white flex justify-center items-center hover:cursor-pointer shadow-md mb-4" x-data="{photoName: null}">
+                            <div class="absolute" x-show="! photoName">
+                                <div class="flex flex-col items-center "> <i class="fa fa-cloud-upload fa-3x text-gray-200"></i> <span class="block text-sm text-gray-400 pb-1">Seret File ke sini</span> <span class="block text-sm text-gray-600 font-normal pb-1">atau</span> <span class="block text-sm text-green-500 font-normal">Browse files</span> </div>
+                            </div>
+                            <div class="absolute" x-show="photoName">
+                                <div class="flex flex-col items-center ">
+                                    <p class="px-10 text-sm text-center text-gray-600" x-text="photoName"></p>
+                                </div>
+                            </div>
+                            <input type="file" class="h-full w-full opacity-0" name="" wire:model="file"
+                            x-ref="file"
+                            x-on:change="
+                                    photoName = $refs.file.files[0].name;" >
+                        </div>
                     </div>
+
                 </div>
 
                 <div class="grid justify-items-center mt-6">
