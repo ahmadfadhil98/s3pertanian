046c91f306c0e3880423fddf129ff9149ccaf297
ahmadfadhil98 <1611522009@student.unand.ac.id>
2022-01-23 12:29:41 +0700

    mau_backup

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -6,21 +6,23 @@ use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class StudentImport implements ToModel,WithHeadingRow
+class StudentImport implements ToModel, WithHeadingRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         // dd($row);
-        return new Student([
+        $student = Student::updateOrCreate([
             'id' => $row['no_bp'],
             'name' => $row['nama_lengkap'],
-            'nim' =>$row['no_bp'],
+            'nim' => $row['no_bp'],
             'beasiswa' => $row['beasiswa']
         ]);
+
+        return $student;
     }
 }
